71057eb971be9ed4e9895713b54cd3b52dd8b5b1
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-05 18:55:01 +0000

    m

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,33 @@
+import java.io.*;
 public class PracticeProblem {
-
+	
 	public static void main(String args[]) {
-
+	
 	}
 
-	
-
+	public static String readFile(String filename){
+		FileReader inputStream= null;
+		String b="";
+		try{
+			inputStream= new FileReader(filename);
+			int c;
+			while ((c = inputStream.read()) != -1) {
+			b = b+(char)c;
+		}
+	}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+	finally {
+			try {
+			if (inputStream != null) {
+				inputStream.close();
+				}
+			}
+			catch(IOException e) {
+				System.out.println(e);
+				}
+	}
+	return b;
+}
 }
